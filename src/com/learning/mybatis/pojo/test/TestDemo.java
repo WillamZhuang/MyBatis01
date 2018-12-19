e373a559eedc5c1c8e260b46e14370a05794175a
chopper <1209377250@qq.com>
2018-12-19 23:38:49 +0800

    to write mappers

---
+++ src/com/learning/mybatis/pojo/test/TestDemo.java
@@ -0,0 +1,24 @@
+package com.learning.mybatis.pojo.test;
+
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+public class TestDemo {
+
+	@Test
+	private void testMethod() throws Exception {
+		//1.创建sqlSessionFactoryBuilder对象
+		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+		//2.加载sqlConfig.xml文件
+		
+		//3.创建sqlSessionFactory对象
+		
+		//4.创建sqlSession对象
+		
+		//5.执行sqlSession对象执行查询，获取结果User
+		
+		//6.打印结果
+		
+		//7.释放资源
+	}
+}
